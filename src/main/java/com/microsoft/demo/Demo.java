ffc704ca9128e98d66cbc43fba3b0410fc181155
vidarlee <vidarlee@foxmail.com>
2021-05-24 13:51:52 +0800

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -1,7 +1,7 @@
 package com.microsoft.demo;
 
 public class Demo {
-    public void DoSomething(boolean flag){
+    public void doSomething(boolean flag){
         if(flag){
             System.out.println("I am covered");
             return;
@@ -9,4 +9,9 @@ public class Demo {
 
         System.out.println("I am not covered");
     }
+    
+   public static void main(String[] args) {
+       Demo demo = new Demo();
+       demo.doSomething(true);
+   }
 }
